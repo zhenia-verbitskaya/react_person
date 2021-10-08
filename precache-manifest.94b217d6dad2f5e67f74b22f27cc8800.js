0965aa9f3e0366addda01c96fb6c3e4c846abafd
Jane Verbitskaya <jvzns17@gmail.com>
2021-10-08 15:31:56 +0300

    Updates

---
+++ precache-manifest.94b217d6dad2f5e67f74b22f27cc8800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c465dc62912eea6d129e8d81c02215d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c6abe385dc997a86787",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6abe385dc997a86787",
+    "url": "./static/js/main.8deec235.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
